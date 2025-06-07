8206aca93993de6cbbf7a0e83783aacbbe0ac16c
GreenWall3 <mikesmit457@gmail.com>
2025-06-07 04:16:27 +0100

    Update page.tsx

--- src/app/cookies/page.tsx
+++ src/app/cookies/page.tsx
@@ -264,7 +264,7 @@ export default function CookiesPage() {
               </div>
               <div className="text-center mt-2">
                 <div className="font-semibold text-black">One Of the Best Resume Builder</div>
-                <div className="text-sm text-gray-500">we Started in 2022 • 2025</div>
+                <div className="text-sm text-gray-500">we Started in 2022</div>
               </div>
             </div>
             
